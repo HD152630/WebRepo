05b9ac1afbb8deda5ec197dc3c31015e16793acd
HD162630 <eclair0111@naver.com>
2017-10-13 18:27:51 +0900

    asdf

--- WebClass/src/org/dimigo/servlet/BlogLogoutServlet.java
+++ WebClass/src/org/dimigo/servlet/BlogLogoutServlet.java
@@ -30,7 +30,6 @@ public class BlogLogoutServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
